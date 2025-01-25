57b87d8ddca215882b88390b32388111410dd407
Fernando Fincatti <fernandocfbf@al.insper.edu.com.br>
2025-01-24 21:07:21 -0300

    feat: adding network limit selecion (max nodes and max depth

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ from pyvis import network
 from src.utils.dataset import get_dataset
 from src.utils.visualization import generate_network
 
-from src.network_analysis import NetworkAnalysis
+from src.network_analysis import NetworkAnalysis, NetworkContext
 from src.transactions import Transactions
 
 st.set_page_config(page_title="AML Analysis", page_icon="src/images/aml.png")
 st.sidebar.title('Anti Money Laudering Analysis')
 
 transactions_df = Transactions().load_data()
-st.sidebar.date_input(label="Period of analysis", format="DD/MM/YYYY", )
-selected_customer = st.sidebar.selectbox('Choose a customer ID to analyse:', set(transactions_df['sender']))
-physics=st.sidebar.checkbox('enable physics', value=True)
-
-customer_transactions = transactions_df.query("sender == @selected_customer or receiver == @selected_customer")
-network_analysis = NetworkAnalysis(customer_transactions, physics)
+date_input = st.sidebar.date_input(label="Period of analysis", format="DD/MM/YYYY")
+selected_account_id = st.sidebar.selectbox('Choose a account ID to analyse:', set(transactions_df['sender']))
+max_depth = st.sidebar.number_input("Select network max depth", value=2, placeholder="Type a number...")
+max_nodes = st.sidebar.number_input("Select network max nodes", max_value=300, value=50, placeholder="Type a number...")
+physics = st.sidebar.checkbox('enable physics', value=False)
+network_context = NetworkContext(transactions_df, physics, selected_account_id, max_depth, max_nodes)
+network_analysis = NetworkAnalysis(network_context)
 network_analysis.generate_network()
 network_html = open("pyvis_network.html", 'r', encoding='utf-8').read()
-components.html(network_html, height=600, width=800)
+with st.container(height=500):
+    components.html(network_html, height=460)
--- src/network_analysis.py
+++ src/network_analysis.py
@@ -1,20 +1,57 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+
 import pandas as pd
 import networkx as nx
-from pyvis import network
+from dataclasses import dataclass
 
 from src.utils.visualization import generate_network
 
-class NetworkAnalysis:
-    def __init__(self, transactions_df: pd.DataFrame, physics: bool):
-        self.transactions_df = transactions_df
-        self.physics = physics
+@dataclass
+class NetworkContext:
+    transactions_df: pd.DataFrame
+    physics: bool
+    account_id: int
+    max_depth: int
+    max_nodes: int
 
+class NetworkAnalysis:
+    def __init__(self, network_context: NetworkContext):
+        self.network_context = network_context
+
+    def _get_network_by_max_nodes(self, depths):
+        limited_nodes = set()
+        for i, node in enumerate(depths.keys()):
+            if i >= self.network_context.max_nodes:
+                break
+            limited_nodes.add(node)
+        return limited_nodes
+    
     def generate_network(self):
-        relationship_network = nx.from_pandas_edgelist(self.transactions_df, source="sender", target="receiver", edge_attr=["title","value"])
+        logging.info("Generating accounts network relationships.")
+        relationship_network = nx.from_pandas_edgelist(
+            self.network_context.transactions_df, 
+            source="sender", 
+            target="receiver", 
+            edge_attr=["title","value"])
+        
+        logging.info("Setting node attributes.")
         degree_dict_G = dict(relationship_network.degree)
         nx.set_node_attributes(relationship_network, degree_dict_G, "value")
-        title_dict_G = {}
+        title_dict_G, color_dict_G  = {}, {}
         for node in list(relationship_network.nodes):
             title_dict_G[node] = f"id: {node}"
+            if node == self.network_context.account_id:
+                color_dict_G[node] = "#4B0082"
+            else:
+                color_dict_G[node] = "#EF9900"
         nx.set_node_attributes(relationship_network, title_dict_G, "title")
-        generate_network(relationship_network, "pyvis_network.html", physics=self.physics)
+        nx.set_node_attributes(relationship_network, color_dict_G, "color")
+
+        logging.info(f"Finding filtered network based on account id {self.network_context.account_id} and max depth {self.network_context.max_depth}")
+        depths = nx.single_source_shortest_path_length(relationship_network, source=self.network_context.account_id, cutoff=self.network_context.max_depth)
+        nodes = self._get_network_by_max_nodes(depths)
+        subgraph = relationship_network.subgraph(nodes)
+        logging.info("Creating network file (HTML).")
+        generate_network(subgraph, "pyvis_network.html", physics=self.network_context.physics)
+        logging.info("File created successfully.")
