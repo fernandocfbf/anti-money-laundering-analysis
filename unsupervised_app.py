578e42ccd5cb3abbd27a58dec2d14a282186c050
Fernando Fincatti <fernandocfbf@al.insper.edu.com.br>
2026-04-02 23:08:09 -0300

    feat: testing streamlit connection integration

--- unsupervised_app.py
+++ unsupervised_app.py
@@ -10,11 +10,11 @@ st.set_page_config(
     layout="wide", 
     page_title="AML Analysis"
 )
-raw_df = get_raw_transactions(DATA_PATH)
+'''raw_df = get_raw_transactions(DATA_PATH)
 raw_df = raw_df.sample(n=1000)
 
 col1, col2, col3 = st.columns([1, 5, 1])
 with col2:
     st.title("Anti Money Laundering Analysis")
-    st.dataframe(raw_df, use_container_width=True)
+    st.dataframe(raw_df, use_container_width=True)'''
 
