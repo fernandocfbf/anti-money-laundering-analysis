d7a6653e19a023e7f917655e3f756379b516c9d1
Fernando Fincatti <fernandocfbf@al.insper.edu.com.br>
2026-05-05 22:57:46 -0300

    feat: cleaning outdated code that was based on the previous project approach

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -9,23 +9,3 @@ def get_raw_transactions(path_to_file:str="src/data/HI-Small_Trans.csv", sample_
             path_to_file, 
             skiprows=lambda i: i > 0 and random.random() > sample_percentage)
     return pd.read_csv(path_to_file, engine="pyarrow")
-
-@st.cache_data
-def get_full_transactions_dataset(visualization=False) -> pd.DataFrame:
-    dataframe = pd.read_csv("src/data/full_transactions_data.csv")
-    if visualization:
-        return dataframe.drop(columns=["is_laundering"])
-    return dataframe
-
-@st.cache_data
-def get_accounts_details_dataset(sample: int=100) -> pd.DataFrame:
-    return pd.read_csv("src/data/accounts_details.csv").head(sample)
-
-@st.cache_data
-def get_network_analysis_dataset(transactions_df: pd.DataFrame) -> pd.DataFrame:
-    network_df = transactions_df.copy()
-    network_df = network_df.groupby(["sender", "receiver"], as_index=False).agg({"amount_paid":  "sum"})
-    network_df.columns = ['sender','receiver','sum']
-    network_df = network_df.rename(columns={"sum": "value"})
-    network_df["title"] = network_df.apply(lambda df: f"from: {df.sender}\nto: {df.receiver}\namount (BRL): {df.value}", axis=1)
-    return network_df
--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,12 +1,4 @@
-from pyvis import network
 import streamlit as st
-from networkx.classes.graph import Graph
-
-def generate_network_vis(network_graph: Graph, filename: str, physics: bool=False) -> None:
-    nt = network.Network(directed=True)
-    nt.from_nx(network_graph)
-    nt.toggle_physics(physics)
-    nt.save_graph(filename)
 
 def generate_info_card(title, value):
     """
